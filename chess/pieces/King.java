e64665c6a679ae247a3fdfef7937adada8c8f452
AuRocKbra <4haurock@gamil.com>
2020-04-06 17:50:13 -0300

    Implementado regra do rouque

--- chess/pieces/King.java
+++ chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece{
+	
+	private ChessMatch cheesMatch;
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.cheesMatch = chessMatch;
 	}
 
 	@Override
@@ -52,11 +56,36 @@ public class King extends ChessPiece{
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()]=true;
 		}
+		if(getMoveCount()==0 && !cheesMatch.getCheck()) {
+			Position postionRook1 = new Position(position.getRow(),position.getColumn()+3);
+			if(testRookCastiling(postionRook1)) {
+				Position p1 = new Position(position.getRow(),position.getColumn()+1);
+				Position p2 = new Position(position.getRow(),position.getColumn()+2);
+				if(getBoard().piece(p1) ==null && getBoard().piece(p2) ==null) {
+					mat[position.getRow()][position.getColumn()+2]=true;
+				}
+			}
+			Position postionRook2 = new Position(position.getRow(),position.getColumn()-4);
+			if(testRookCastiling(postionRook2)) {
+				Position p1 = new Position(position.getRow(),position.getColumn()-1);
+				Position p2 = new Position(position.getRow(),position.getColumn()-2);
+				Position p3 = new Position(position.getRow(),position.getColumn()-3);
+				if(getBoard().piece(p1) ==null && getBoard().piece(p2) ==null && getBoard().piece(p3)==null) {
+					mat[position.getRow()][position.getColumn()-2]=true;
+				}
+			}
+		}
+		
 		return mat;
 	}
 	
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p==null || p.getColor()!=getColor();
+	}
+	
+	private boolean testRookCastiling(Position postion) {
+		ChessPiece p = (ChessPiece)getBoard().piece(postion);
+		return p != null && p instanceof Rook && p.getColor() == getColor()	&& p.getMoveCount()==0;
 	}
 }
